d0e2b964ec03368f28a9a16e0942c9363dcf93bf
delete <rickagent478554@gmail.com>
2026-06-05 22:29:43 -0600

    Cambio de credenciales de la DB

--- POS_E1_ISW/src/main/java/com/tienda/dao/Conexion.java
+++ POS_E1_ISW/src/main/java/com/tienda/dao/Conexion.java
@@ -6,10 +6,10 @@ import java.sql.SQLException;
 
 public class Conexion {
     // Parámetros de MySQL para hacer la conexión
-    private static final String DATABASE = "isw_schema"; //Nombre de la base de datos, No de la conexión
+    private static final String DATABASE = "db_isw"; //Nombre de la base de datos, No de la conexión
     private static final String URL = "jdbc:mysql://localhost:3306/" + DATABASE + "?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";//URL de conexión 
     private static final String USER = "root"; // Usuario de MySQL
-    private static final String PASSWORD = "R3cD4D4t4b4s3@$"; // Contraseña de la conexión
+    private static final String PASSWORD = "root_password_isw"; // Contraseña de la conexión
 
     // Método para la obtención de la conexión
     public static Connection getConexion() {
