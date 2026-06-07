25f850a8b033a53774c28b1a19313e50b380a21b
ItsMeRecSevenTeen <rickagent478554@gmail.com>
2026-06-07 15:30:42 -0600

    Se agregó inicializadores para inyectar las fuentes si el sistema no las tiene instaladas, se cambió el tema en modo claro

--- POS_E1_ISW/src/main/java/com/tienda/POS_E1_ISW.java
+++ POS_E1_ISW/src/main/java/com/tienda/POS_E1_ISW.java
@@ -4,36 +4,77 @@
 
 package com.tienda;
 
+//import com.formdev.flatlaf.FlatLightLaf;
+//import com.formdev.flatlaf.themes.FlatMacDarkLaf;
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+
 import com.tienda.dao.Conexion;
 import java.sql.Connection;
-import com.formdev.flatlaf.FlatLightLaf; // Importante importar la librería
 import com.tienda.vista.MainFrame;
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+import java.io.InputStream;
+import java.sql.SQLException;
+import javax.swing.UIManager;
 /**
  *
  * @author Rec17
  */
 public class POS_E1_ISW {
 
-    public static void main(String[] args) {
-        // Test de conexión instantáneo
+    public static void main(String[] args) throws SQLException {
+        // Carga de fuentes
+        try {
+            // La raíz es src -------------------------------------v
+            InputStream is = POS_E1_ISW.class.getResourceAsStream("/fonts/DearSans-Book.ttf");
+
+            if (is != null) {
+                Font fuenteCustom = Font.createFont(Font.TRUETYPE_FONT, is);
+
+                // Registrar en el sistema operativo virtual de Java
+                GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+                ge.registerFont(fuenteCustom);
+
+                // Obtener el nombre exacto de la fuente
+                System.out.println("El nombre real de la fuente es: " + fuenteCustom.getName());
+
+            } else {
+                System.err.println("ERROR: No se encontro el archivo TTF, Revisa que este dentro de src/main/resources/fonts/");
+            }
+        } catch (Exception e) {
+            System.err.println("Error critico al registrar la fuente: " + e.getMessage());
+        }
+
+        // Test de conexion a la db
         System.out.println("Intentando conectar a MySQL...");
         Connection cn = Conexion.getConexion();
-        
-        if (cn != null) {
-            System.out.println("¡Conexión EXITOSA! El sistema está listo.");
-            try { cn.close(); } catch (Exception e) {} // Cerramos la prueba
-        } else {
-            System.out.println("Error crítico: Revisa tus credenciales o enciende tu servidor.");
-        }
-        // 1. Activar el Look and Feel antes de abrir cualquier ventana
-        FlatLightLaf.setup(); // Esto activa el modo claro moderno
-        
-        // Si prefieren el modo oscuro, cambien la línea de arriba por:
-        com.formdev.flatlaf.FlatDarkLaf.setup();
 
-        // 2. Aquí ya lanzan su primer panel o ventana
+        if (cn != null) {
+            System.out.println("Conexion exitosa");
+            try {
+                cn.close();
+            } catch (SQLException e) {
+            } // Cerrar la prueba
+        } else {
+            System.out.println("Error critico, Revisa la conexion del Docker");
+        }
+
+        //Modo claro para todos los frames
+        try {
+            UIManager.setLookAndFeel(new FlatMacLightLaf());
+        } catch (Exception ex) {
+            System.err.println("Error al iniciar FlatLaf");
+        }
+        // Modo oscuro para todos los frames
+        //try {
+        //    UIManager.setLookAndFeel( new FlatMacDarkLaf() );
+        //} catch( Exception ex ) {
+        //    System.err.println( "Error al iniciar FlatLaf" );
+        //}
+
+        // Invocando a ventana main
         java.awt.EventQueue.invokeLater(() -> {
-            new MainFrame().setVisible(true); // O la ventana que tengan armada
+            new MainFrame().setVisible(true);
         });
     }
 }
