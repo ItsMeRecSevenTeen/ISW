d11294c37712afb34d7d7948f3116d0db91f0d55
ItsMeRecSevenTeen <rickagent478554@gmail.com>
2026-06-07 15:39:57 -0600

    Se agregaron las credenciales para que la clase 'Conexión.java' pueda conectarse y realizar consultas correctamente a la base de datos

--- POS_E1_ISW/src/main/java/com/tienda/dao/Conexion.java
+++ POS_E1_ISW/src/main/java/com/tienda/dao/Conexion.java
@@ -5,23 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-    // Parámetros de MySQL para hacer la conexión
-    private static final String DATABASE = "db_isw"; //Nombre de la base de datos, No de la conexión
-    private static final String URL = "jdbc:mysql://localhost:3306/" + DATABASE + "?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";//URL de conexión 
-    private static final String USER = "root"; // Usuario de MySQL
-    private static final String PASSWORD = "root_password_isw"; // Contraseña de la conexión
+    private static final String URL = "jdbc:mysql://localhost:3306/db_isw";
+    private static final String USER = "root";
+    private static final String PASS = "root_password_isw";
 
-    // Método para la obtención de la conexión
-    public static Connection getConexion() {
-        Connection conectar = null;
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conectar = DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (ClassNotFoundException e) {
-            System.err.println("Error: No se encontró el Driver de MySQL. " + e.getMessage());
-        } catch (SQLException e) {
-            System.err.println("Error: Falló la conexión a la base de datos. Verificar si el servidor está encendido. " + e.getMessage());
-        }
-        return conectar;
+    public static Connection getConexion() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
     }
 }
