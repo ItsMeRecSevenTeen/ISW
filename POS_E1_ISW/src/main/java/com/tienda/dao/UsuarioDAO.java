8309324b55ff03c4955a418db60bc19dbb5acfb0
ItsMeRecSevenTeen <rickagent478554@gmail.com>
2026-06-07 15:49:30 -0600

    Se creó el método 'autenticar()' para que 'AdminLoginPanel.java' y 'CajeroLoginPanel.java' puedan consultar las credenciales correspondientes ingresadas a la DB

--- POS_E1_ISW/src/main/java/com/tienda/dao/UsuarioDAO.java
+++ POS_E1_ISW/src/main/java/com/tienda/dao/UsuarioDAO.java
@@ -8,6 +8,31 @@ package com.tienda.dao;
  *
  * @author Rec17
  */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UsuarioDAO {
-    
+
+    public int autenticar(String usuario, String password) {
+        String sql = "SELECT rol FROM usuario WHERE nombre_usuario = ? AND contrasena_hash = SHA2(?, 256) AND activo = true";
+
+        try (Connection conn = Conexion.getConexion(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, usuario);
+            pstmt.setString(2, password);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getInt("rol"); // Retorna 0 (Admin) o 1 (Cajero)
+                }
+                return -1; // Credenciales incorrectas o usuario inactivo
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Error en UsuarioDAO.autenticar: " + e.getMessage());
+            return -1; // En caso de error de BD, asumimos que no pasa
+        }
+    }
 }
