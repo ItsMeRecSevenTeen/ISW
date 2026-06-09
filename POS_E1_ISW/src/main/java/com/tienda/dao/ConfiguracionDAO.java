836db2e71e32b913e9ff932503885402de74e1f7
ItsMeRecSevenTeen <rickagent478554@gmail.com>
2026-06-09 05:26:56 -0600

    .

--- POS_E1_ISW/src/main/java/com/tienda/dao/ConfiguracionDAO.java
+++ POS_E1_ISW/src/main/java/com/tienda/dao/ConfiguracionDAO.java
@@ -8,6 +8,13 @@ package com.tienda.dao;
  *
  * @author Rec17
  */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class ConfiguracionDAO {
-    
+    public void modificarIVA(){
+        
+    }
 }
