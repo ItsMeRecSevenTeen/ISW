31707f35fd0be4083944ef41283af3b0bcc499bb
ItsMeRecSevenTeen <rickagent478554@gmail.com>
2026-06-13 15:30:41 -0600

    fix: Diseño corregido en AperturaCaja.form
    - Se alineó el texto central del nombre del cajero
    - Se agregó el icono de 'check.svg' dentro del botón de enviar
    - Se le agregó un placeholder al campo de texto de dinero

--- POS_E1_ISW/src/main/java/com/tienda/vista/AperturaCaja.java
+++ POS_E1_ISW/src/main/java/com/tienda/vista/AperturaCaja.java
@@ -4,13 +4,35 @@
  */
 package com.tienda.vista;
 import com.tienda.dao.TurnoCajaDAO;
+import com.tienda.dao.UsuarioDAO;
+import com.tienda.util.Sesion;
 import javax.swing.JOptionPane;
 
 public class AperturaCaja extends javax.swing.JPanel {
-
+    UsuarioDAO usuarioDao = new UsuarioDAO();
     public AperturaCaja() {
         initComponents();
-       // jLabel1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+       // Asociar la tecla enter con envío de credenciales
+        jTextField1.addActionListener(e -> jButton2ActionPerformed(null));
+        
+        jButton1.setIcon(new com.formdev.flatlaf.extras.FlatSVGIcon("icons/backward.svg", 24, 24));
+        jLabel1.putClientProperty("FlatLaf.style", "font: 56 'DearSans-Book'");
+        jLabel1.setIcon(new com.formdev.flatlaf.extras.FlatSVGIcon("icons/user.svg", (float) 6.0));
+        // Centrar el contenido (icono + texto) horizontalmente
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        // Mover el texto abajo del icono
+        jLabel1.setVerticalTextPosition(javax.swing.SwingConstants.CENTER);
+        jLabel1.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
+        // Icono de checkmark para enviar la contraseña
+        jButton2.putClientProperty("JButton.buttonType", "roundRect");
+        jButton2.setIcon(new com.formdev.flatlaf.extras.FlatSVGIcon("icons/check.svg", 24, 24));
+        //placeholder
+        jTextField1.putClientProperty("JTextField.placeholderText", "Ingrese efectivo en caja");
+        // Saca el nombre de la sesión actual
+        String cajeroActual = Sesion.getInstancia().getNombreUsuario();
+
+        // Escribir el nombre de la sesión actual
+        jLabel1.setText(cajeroActual);
     }
 
     @SuppressWarnings("unchecked")
@@ -22,9 +44,9 @@ public class AperturaCaja extends javax.swing.JPanel {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextPane1 = new javax.swing.JTextPane();
         jLabel1 = new javax.swing.JLabel();
-        efectivoEnCaja = new javax.swing.JTextField();
+        jTextField1 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
-        atras = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
 
         button1.setLabel("button1");
 
@@ -34,71 +56,53 @@ public class AperturaCaja extends javax.swing.JPanel {
         setPreferredSize(new java.awt.Dimension(500, 500));
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setFont(new java.awt.Font("Helvetica Neue", 1, 36)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("Cajero");
-        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 210, -1, -1));
+        jLabel1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        jLabel1.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
+        add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 140, 510, 400));
 
-        efectivoEnCaja.setFont(new java.awt.Font("Helvetica Neue", 0, 14)); // NOI18N
-        efectivoEnCaja.setText("ingresar efectivo en caja");
-        efectivoEnCaja.addActionListener(this::efectivoEnCajaActionPerformed);
-        add(efectivoEnCaja, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 260, 200, 30));
+        jTextField1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jTextField1.addActionListener(this::jTextField1ActionPerformed);
+        add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 540, 240, -1));
 
-        jButton1.setText("jButton1");
+        jButton1.setText("Atrás");
+        jButton1.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        jButton1.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
         jButton1.addActionListener(this::jButton1ActionPerformed);
-        add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 260, 25, -1));
+        add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 50, 100, 40));
 
-        atras.setText("Atrás");
-        atras.addActionListener(this::atrasActionPerformed);
-        add(atras, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, -1, -1));
+        jButton2.addActionListener(this::jButton2ActionPerformed);
+        add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(720, 540, 40, 40));
     }// </editor-fold>//GEN-END:initComponents
 
-    private void efectivoEnCajaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_efectivoEnCajaActionPerformed
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_efectivoEnCajaActionPerformed
+    }//GEN-LAST:event_jTextField1ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-String textoMonto=efectivoEnCaja.getText().trim();
-        if(textoMonto.isEmpty()){
-            JOptionPane.showMessageDialog(this, "Ingresar el monto de efectivo para continuar.",
-            "Campo requerido",JOptionPane.WARNING_MESSAGE);
-            efectivoEnCaja.requestFocus();
-            return;
-        }
-        try{
-            double monto=Double.parseDouble(textoMonto);
-            if(monto < 0 ){
-                JOptionPane.showMessageDialog(this, "El monte inicial no puede ser negativo.",
-                        "Monto inválido",JOptionPane.ERROR_MESSAGE);
-                efectivoEnCaja.requestFocus();
-                return;
-            }
-            TurnoCajaDAO turnoDAO=new TurnoCajaDAO();
-            turnoDAO.registrarApertura(monto);
-            efectivoEnCaja.setText("");
-            
-        }catch (NumberFormatException e){
-            JOptionPane.showMessageDialog(this,"Ingresar un monto válido",
-            "Error de formato",JOptionPane.ERROR_MESSAGE);
-            efectivoEnCaja.requestFocus();
+        // Obtener la info del frame principal
+        MainFrame framePrincipal = (MainFrame) javax.swing.SwingUtilities.getWindowAncestor(this);
+        if (framePrincipal != null) {
+            // Instanciar el nuevo panel del Login de Administrador
+            LoginPanel LoginPanel = new LoginPanel(); //Para cada ventana, cambiar su constructor
+
+            // Mandar a hacer el intercambio
+            framePrincipal.cambiarPanel(LoginPanel);
         }
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    private void atrasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_atrasActionPerformed
-        MainFrame framePrincipal=(MainFrame)javax.swing.SwingUtilities.getWindowAncestor(this);
-        if (framePrincipal != null){
-            CajeroLoginPanel panelCajero=new CajeroLoginPanel();
-            framePrincipal.cambiarPanel(panelCajero);
-        }
-    }//GEN-LAST:event_atrasActionPerformed
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton atras;
     private java.awt.Button button1;
-    private javax.swing.JTextField efectivoEnCaja;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField jTextField1;
     private javax.swing.JTextPane jTextPane1;
     // End of variables declaration//GEN-END:variables
 }
