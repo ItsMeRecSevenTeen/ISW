526df0bcd1ea0ffe1cbcfd83c5efd8881f0292e0
ItsMeRecSevenTeen <rickagent478554@gmail.com>
2026-06-13 15:23:28 -0600

    feat: Implementación Singleton
    - Se creó una nueva clase para mantener la sesión actual, esto por que
      diseño requiere que la ventana 'AperturaCaja.form' debe mostrar el
      nombre del usuario actual.

---
+++ POS_E1_ISW/src/main/java/com/tienda/util/Sesion.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda.util;
+
+/**
+ *
+ * @author Rec17
+ */
+public class Sesion {
+    private static Sesion instancia;
+    private String nombreUsuario;
+    private int rol; // Por si se necesita que admin o cajero lo use
+
+    // Constructor privado para que no usen el método afuera
+    private Sesion() {}
+
+    // Punto de acceso global
+    public static Sesion getInstancia() {
+        if (instancia == null) {
+            instancia = new Sesion();
+        }
+        return instancia;
+    }
+
+    // Getters y Setters
+    public String getNombreUsuario() { return nombreUsuario; }
+    public void setNombreUsuario(String nombreUsuario) { this.nombreUsuario = nombreUsuario; }
+
+    public int getRol() { return rol; }
+    public void setRol(int rol) { this.rol = rol; }
+
+    // Método para que cierre sesión
+    public void cerrarSesion() {
+        nombreUsuario = null;
+        rol = 0;
+    }
+}
